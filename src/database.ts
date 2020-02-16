f1a0fbd1b13643998d88c02ea7b4033ecacb925e
luiyivp <31863570+luiyivp@users.noreply.github.com>
2020-02-16 19:53:22 -0400

    first commit

---
+++ src/database.ts
@@ -0,0 +1,9 @@
+import { connect } from 'mongoose';
+
+export async function startConnection() {
+    const db = await connect('mongodb://localhost/photo-gallery-db', {
+        useNewUrlParser: true,
+        useFindAndModify: false
+    });
+    console.log('Database is connected');
+}
---
+++ src/routes/index.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+const router = Router();
+
+import upload from '../libs/multer';
+import {
+    getPhotos,
+    createPhoto,
+    deletePhoto,
+    getPhoto,
+    updatePhoto
+} from '../controllers/photo.controller';
+
+// routes
+router
+    .route('/photos')
+    .get(getPhotos)
+    .post(upload.single('image'), createPhoto);
+
+router
+    .route('/photos/:id')
+    .get(getPhoto)
+    .delete(deletePhoto)
+    .put(updatePhoto);
+
+export default router;
